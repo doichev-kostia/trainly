7d8f38700110f3c59d161ba300c2c1061f2091be
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-10-08 16:33:28 +0200

    feat: api init

---
+++ apps/api/src/constants.ts
@@ -0,0 +1,20 @@
+export const milliseconds = {
+	second: 1000,
+	minute: 60 * 1000,
+	hour: 60 * 60 * 1000,
+	day: 24 * 60 * 60 * 1000,
+	week: 7 * 24 * 60 * 60 * 1000,
+} as const;
+
+export const StatusCodes = {
+	OK: 200,
+	CREATED: 201,
+	ACCEPTED: 202,
+	NO_CONTENT: 204,
+	BAD_REQUEST: 400,
+	UNAUTHORIZED: 401,
+	FORBIDDEN: 403,
+	NOT_FOUND: 404,
+	CONFLICT: 409,
+	INTERNAL_SERVER_ERROR: 500,
+} as const;
