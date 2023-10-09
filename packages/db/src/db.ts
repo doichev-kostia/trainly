504cb520354f83f5f5cc1a58e6964e9ae2428fc0
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-10-09 06:21:27 +0200

    feat: db updates

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,27 +1,26 @@
 import { type DB } from "./types.js";
 
-import { Pool } from "pg";
-import { Kysely, PostgresDialect } from "kysely";
+import pg from "pg";
+import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely";
 import { parseEnv } from "@trainly/utils";
 import { z } from "zod";
 
-parseEnv({
+const schema = z.object({
 	DATABASE_URL: z.string().url(),
 });
 
+parseEnv(schema);
+
 const dialect = new PostgresDialect({
-	pool: new Pool({
+	pool: new pg.Pool({
 		connectionString: process.env.DATABASE_URL,
 		max: 10,
 	}),
 });
 
-// Database interface is passed to Kysely's constructor, and from now on, Kysely
-// knows your database structure.
-// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
-// to communicate with your database.
 export const db = new Kysely<DB>({
 	dialect,
+	plugins: [new CamelCasePlugin()],
 });
 
 export * from "./types.js";
