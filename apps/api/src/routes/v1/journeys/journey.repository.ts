4eff5a7e00663f81eefb6684764e69f0d0b5dd06
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-11-06 19:19:47 +0100

    feat: list jouneys

--- apps/api/src/routes/v1/journeys/journey.repository.ts
+++ apps/api/src/routes/v1/journeys/journey.repository.ts
@@ -1,19 +1,108 @@
-import { type Journey } from "@trainly/db/schema/journeys";
-import { type Route } from "@trainly/db/schema/routes";
+import { type Journey, journeys } from "@trainly/db/schema/journeys";
+import { type Route, routes } from "@trainly/db/schema/routes";
 import { type JourneyStop } from "@trainly/db/schema/journey-stops";
-import { type Seat } from "@trainly/db/schema/seats";
+import { type Seat, seats } from "@trainly/db/schema/seats";
+import { type ListResponse } from "@trainly/contracts";
+import { BaseRepository } from "#base-repository";
+import { and, db, eq, gt, gte, inArray, sql } from "@trainly/db";
+import { stops } from "@trainly/db/schema/stops";
+import { stations } from "@trainly/db/schema/stations";
+import { platforms } from "@trainly/db/schema/platforms";
+import { relations } from "~/utils/db.js";
 
 type RetrievedJourney = Journey & {
 	route?: Route;
 	stops?: JourneyStop[];
 	seats?: Seat[];
 };
+
+type ListParams = {
+	limit?: number;
+	offset?: number;
+	expand?: string[];
+};
+
 export class JourneyRepository {
 	private static instance: JourneyRepository | undefined;
 
+	private base: BaseRepository<typeof journeys, typeof db>;
+
+	private constructor(database: typeof db, table: typeof journeys) {
+		this.base = new BaseRepository(table, database);
+	}
+
 	public static getInstance() {
 		if (!JourneyRepository.instance) {
-			JourneyRepository.instance = new JourneyRepository();
+			JourneyRepository.instance = new JourneyRepository(db, journeys);
 		}
+
+		return JourneyRepository.instance;
+	}
+
+	public async listAvailable(
+		departureCity: string,
+		arrivalCity: string,
+		departureDate: Date,
+		minSeats: number,
+		params?: ListParams,
+	): Promise<ListResponse<RetrievedJourney>> {
+		const { table } = this.base;
+		const { limit, offset, expand = [] } = params ?? {};
+
+		const departure = db.$with("departure").as(
+			db
+				.select({
+					stopOrder: stops.order,
+					routeId: routes.id,
+				})
+				.from(stations)
+				.innerJoin(platforms, eq(platforms.stationId, stations.id))
+				.innerJoin(stops, eq(stops.platformId, platforms.id))
+				.innerJoin(routes, eq(routes.id, stops.routeId))
+				.where(eq(stations.name, departureCity)),
+		);
+
+		const arrival = db.$with("arrival").as(
+			db
+				.with(departure)
+				.select({
+					routeId: routes.id,
+				})
+				.from(stations)
+				.innerJoin(platforms, eq(platforms.stationId, stations.id))
+				.innerJoin(stops, eq(stops.platformId, platforms.id))
+				.innerJoin(routes, eq(routes.id, stops.routeId))
+				.innerJoin(departure, eq(departure.routeId, routes.id))
+				.where(and(eq(stations.name, arrivalCity), gt(stops.order, departure.stopOrder))),
+		);
+
+		const journeys = await db
+			.with(arrival)
+			.select({
+				id: table.id,
+			})
+			.from(table)
+			.innerJoin(routes, eq(table.routeId, routes.id))
+			.innerJoin(arrival, eq(arrival.routeId, routes.id))
+			.innerJoin(seats, eq(table.id, seats.journeyId))
+			.where(gte(table.departureTime, departureDate))
+			.groupBy(table.id)
+			.having(sql`COUNT(${seats.id}) >= ${minSeats}`);
+
+		const journeyIds = journeys.map((j) => j.id);
+
+		const data = await db.query.journeys.findMany({
+			where: inArray(table.id, journeyIds),
+			with: relations(expand),
+			limit,
+			offset,
+		});
+
+		console.dir({ data }, { depth: null });
+
+		return {
+			items: data,
+			count: journeyIds.length,
+		};
 	}
 }
--- apps/api/src/routes/v1/journeys/routes.ts
+++ apps/api/src/routes/v1/journeys/routes.ts
@@ -1,5 +1,22 @@
 import { type Instance } from "~/utils/types.js";
+import { JourneyResponseSchema, ListJourneysQuerySchema } from "@trainly/contracts/journeys";
+import { ListResponseSchema } from "@trainly/contracts";
+import { listAvailableJourneys } from "./handlers/list-available-journeys.js";
 
-async function journeyRoutes(fastify: Instance, options) {}
+async function journeyRoutes(fastify: Instance) {
+	fastify.addHook("onRequest", fastify.verifyAuthToken);
+
+	fastify.route({
+		method: "GET",
+		url: "/",
+		schema: {
+			querystring: ListJourneysQuerySchema,
+			response: {
+				"2xx": ListResponseSchema(JourneyResponseSchema),
+			},
+		},
+		handler: listAvailableJourneys,
+	});
+}
 
 export default journeyRoutes;
