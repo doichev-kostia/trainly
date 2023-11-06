30cbb39dd3f0713c6aea920b88a5b6272b8d16a1
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-11-06 19:19:24 +0100

    fix: relations builder

--- apps/api/src/utils/db.ts
+++ apps/api/src/utils/db.ts
@@ -13,22 +13,24 @@ export function stripValues<T extends object>(object: T) {
 
 /**
  * Transforms an array of relations into a tree structure needed for the ORM.
- * @example ['journey', 'route.train'] => { journey: true, route: { train: true } }
+ * @example ['journey', 'route.train'] => { journey: true, route: { with: { train: true } }}
  * @param expansion
  */
-export function relations(expansion: string[]): Record<string, true | Record<string, true>> {
-	const result: Record<string, true | Record<string, true>> = {};
+export function relations(
+	expansion: string[],
+): Record<string, true | { with: Record<string, true> }> {
+	const result: Record<string, true | { with: Record<string, true> }> = {};
 
 	for (const relation of expansion) {
 		const keys = relation.split(".");
 		const last = keys.pop() as string;
 		let current = result;
 		for (const key of keys) {
-			if (!current[key]) {
-				current[key] = {};
+			if (!current[key] || typeof current[key] === "boolean") {
+				current[key] = { with: {} };
 			}
 
-			current = current[key] as Record<string, true | Record<string, true>>;
+			current = (current[key] as { with: Record<string, true> }).with;
 		}
 		current[last] = true;
 	}
