970c2a9c033051ea3d2c7311115deed58bee5dc6
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-11-14 22:13:51 +0100

    feat: use env vars

--- apps/web/src/api/client.ts
+++ apps/web/src/api/client.ts
@@ -1,8 +1,10 @@
 import { API } from "@trainly/api-client";
 import { PRIVATE_API_KEY } from "$env/static/private";
+import { PUBLIC_API_HOST, PUBLIC_API_PROTOCOL, PUBLIC_API_PORT } from "$env/static/public";
+import { z } from "zod";
 
 export const api = new API(PRIVATE_API_KEY, {
-	host: "localhost",
-	port: 8080,
-	protocol: "http",
+	host: PUBLIC_API_HOST,
+	port: Number(PUBLIC_API_PORT.trim()),
+	protocol: z.enum(["http", "https"]).parse(PUBLIC_API_PROTOCOL),
 });
